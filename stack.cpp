e8de111bc76dcf747ecf429e1a9fe79b5ae2860a
marchdillo <kevinno.limiy@gmail.com>
2025-06-05 11:26:00 +0700

    menambahkan class public stack

--- stack.cpp
+++ stack.cpp
@@ -17,4 +17,20 @@ class Stack
 {
 private:
     Node *top; // Pointer to the top node of the stack
+
+public:
+    Stack()
+    {
+        top = NULL;
+    }
+
+    int push(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push value: " << value << endl;
+        return value;
+    }
 };
