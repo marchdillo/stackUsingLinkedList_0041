1afc03dc7423d95613470464623387088b9c5b46
marchdillo <kevinno.limiy@gmail.com>
2025-06-05 11:50:50 +0700

    menambahkan while choice case 4

--- stack.cpp
+++ stack.cpp
@@ -97,6 +97,9 @@ int main()
         case 3:
             stack.peek();
             break;
+        case 4:
+            cout << "Exiting program." << endl;
+            break;
         }
     }
 }
